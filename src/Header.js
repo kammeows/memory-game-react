7246f85abfc5cd4784b6a40c88738fed284107e1
Ku7eKam <twrkamakshi@gmail.com>
2024-07-03 16:13:04 +0530

    updated package.json

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header(props) {
   return (
     <div className="header">
-      <span className="header-left">Snake Game</span>
+      <span className="header-left">Memory Game</span>
       <div className="header-right">
         Score: <span className="score">{props.score}</span>
       </div>
